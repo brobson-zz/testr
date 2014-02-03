2e2c703d81d06e0706a85e511a7ca9b43ede39a5
Benjamin Robson <P12215420@GH582-014.lec-admin.dmu.ac.uk>
2014-02-03 11:24:53 +0000

    changed comment line to test branch merge conflictions

--- maths.c
+++ maths.c
@@ -4,6 +4,8 @@ int main() {
 	
 	//this is the line causing issue in the conflict branch
 	
+	//this is the second line causing issues in the conflict branch
+	
 	int x, y, z, a, b =0;
 	int avg;
 	
