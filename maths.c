dc478db0ca455423b6ba90ba47dc57c7aafd369c
Benjamin Robson <P12215420@GH582-014.lec-admin.dmu.ac.uk>
2014-02-03 11:20:38 +0000

    added comment line to test branch merge conflictions

---
+++ maths.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	
+	//this is the line causing issue in the master branch
+	
+	int x, y, z, a, b =0;
+	int avg;
+	
+	printf("Enter values: ");
+	scanf("%i %i %i %i %i", &x, &y);
+	
+	avg = x+y+z+a+b;
+	avg = avg/5;
+	
+	printf(" answer = %i", avg); 
+	printf("this change");
+}
